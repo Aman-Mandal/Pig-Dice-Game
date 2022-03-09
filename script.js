0b48668bf8f758503dd267a2468751912cf0c6f4
Aman-Mandal <002amankumar@gmail.com>
2022-03-09 11:44:15 +0530

    Bug fixed

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer]
 
     // 2. Check if the player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       isPlaying = false
       // remove dice
